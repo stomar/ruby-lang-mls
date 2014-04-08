062e7dfc0892c5d266ad653e86954f9f13f485ed
Marcus Stollsteimer <sto.mar@web.de>
2014-04-08 22:25:13 +0200

    Improve logger tests

--- test/test_mllogger.rb
+++ test/test_mllogger.rb
@@ -9,31 +9,44 @@ describe MLLogger do
     @db_file = File.expand_path(File.dirname(__FILE__) + '/test.db')
     @mllogger = MLLogger.new(:database_url => "sqlite3://#{@db_file}")
 
-    @time = Time.utc(2013, 1, 2, 3, 4, 5)
-    @info = { :status => 'Success', :list => 'ruby-talk', :action => 'test' }
+    @time1 = Time.utc(2013, 1, 2, 3, 4, 5)
+    @time2 = Time.utc(2013, 1, 2, 3, 4, 6)
+    @info1 = { :status => 'Success', :list => 'ruby-talk', :action => 'test' }
+    @info2 = { :status => 'Success', :list => 'ruby-core', :action => 'subscribe' }
   end
 
   it 'can log an entry' do
-    time = Time.utc(2013, 2, 2, 3, 4, 5)
-    info = { :status => 'Success', :list => 'ruby-core', :action => 'subscribe' }
-
     capture_io do
-      Time.stub(:now, @time) { @mllogger.log(@info) }
-      Time.stub(:now, time)  { @mllogger.log(info)  }
+      @mllogger.log(@info1)
     end
 
-    last = @mllogger.entries.split("\n").last
-    last.must_equal "[2013-02-02 03:04:05 +0000] Success (ruby-core, subscribe)"
+    @mllogger.entries.split("\n").last.must_match /Success \(ruby-talk, test\)/
   end
 
-  it 'can return all entries' do
+  it 'can return all entries in correct order' do
     capture_io do
-      @mllogger.log(@info)
-      @mllogger.log(@info)
-      @mllogger.log(@info)
+      Time.stub(:now, @time2) do
+        @mllogger.log(@info2)
+      end
+      Time.stub(:now, @time1) do
+        45.times { @mllogger.log(@info1) }
+      end
     end
+    @mllogger.entries.split("\n").size.must_equal 46
+    @mllogger.entries.split("\n").last.must_match /2013-01-02 03:04:06/
+  end
 
-    @mllogger.entries.split("\n").size.must_equal 3
+  it 'can return recent entries in correct order' do
+    capture_io do
+      Time.stub(:now, @time2) do
+        @mllogger.log(@info2)
+      end
+      Time.stub(:now, @time1) do
+        42.times { @mllogger.log(@info1) }
+      end
+    end
+    @mllogger.recent_entries.split("\n").size.must_equal 40
+    @mllogger.recent_entries.split("\n").last.must_match /2013-01-02 03:04:06/
   end
 
   after do
