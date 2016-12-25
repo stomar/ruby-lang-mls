237c7743695fa9f2625ea9594b79e902632c62b6
Marcus Stollsteimer <sto.mar@web.de>
2016-12-25 18:00:43 +0100

    Add tests for StatsHandler

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -1,4 +1,6 @@
-fixtures = [
+fixtures = Hash.new
+
+fixtures[:log] = [
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 2), status: "Success", list: "ruby-talk", action: "test" },
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 3), status: "Error",   list: "ruby-talk", action: "test_error" },
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 6), status: "Success", list: "ruby-talk", action: "test_last" },
@@ -7,4 +9,10 @@ fixtures = [
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 5), status: "Success", list: "ruby-talk", action: "test" }
 ]
 
-fixtures.each {|row| Log.create(row) }
+fixtures[:dailystats] = [
+  { date: Date.new(2000, 1, 2), talk_subsc: 4 },
+  { date: Date.new(2000, 1, 1), talk_subsc: 7 }
+]
+
+fixtures[:log].each {|row| Log.create(row) }
+fixtures[:dailystats].each {|row| DailyStats.create(row) }
---
+++ test/test_statshandler.rb
@@ -0,0 +1,48 @@
+require_relative "helper"
+
+
+describe MLS::StatsHandler do
+
+  before do
+    setup_database
+    @stats = MLS::StatsHandler.new
+  end
+
+  after do
+    teardown_database
+  end
+
+  it "can increment an existing entry (1)" do
+    @stats.increment("ruby-talk", "subscribe", timestamp: Time.utc(2000, 1, 2, 12, 0, 10))
+
+    @stats.entries.last.must_match /2000-01-02,5/
+  end
+
+  it "can increment an existing entry (2)" do
+    @stats.increment("ruby-core", "unsubscribe", timestamp: Time.utc(2000, 1, 2, 12, 0, 10))
+
+    @stats.entries.last.must_match /2000-01-02,4,0,0,1/
+  end
+
+  it "can increment a non-existing entry" do
+    @stats.increment("ruby-talk", "subscribe", timestamp: Time.utc(2010, 1, 1))
+
+    @stats.entries.last.must_match /2010-01-01,1/
+  end
+
+  it "can increment a non-existing entry for today" do
+    now = Time.utc(2010, 1, 1)
+    Time.stub(:now, now) do
+      @stats.increment("ruby-talk", "unsubscribe")
+    end
+
+    @stats.entries.last.must_match /2010-01-01,0,1/
+  end
+
+  it "can return all entries in correct order" do
+    @stats.entries.size.must_equal 3
+    @stats.entries[0].must_match /date,talk_subsc/
+    @stats.entries[1].must_match /2000-01-01,7/
+    @stats.entries[2].must_match /2000-01-02,4/
+  end
+end
