f8ec3fbb7d1ddbcca46b36a56d4cfb4ce09c5c74
Marcus Stollsteimer <sto.mar@web.de>
2019-02-11 21:40:59 +0100

    Add test for MailerStats

---
+++ test/test_mailerstats.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "../lib/mls/mailerstats"
+
+
+describe MLS::MailerStats do
+
+  before do
+    @mailerstats = MLS::MailerStats.new(api_url: "foo")
+  end
+
+  it "can create a mailer stats report from retrieved data" do
+    response = { "Data" => [{ "DeliveredCount" => 7 }] }
+
+    @mailerstats.stub(:request_data, response) do
+      @mailerstats.get.must_match /\ASent emails today: +7\Z/
+    end
+  end
+end
