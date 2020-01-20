387445fff86de15946c89444ae452465b17e3707
Marcus Stollsteimer <sto.mar@web.de>
2020-01-20 20:18:05 +0100

    Update copyright information

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@
 #
 # project home page: https://github.com/stomar/ruby-lang-mls
 #
-# Copyright (C) 2013-2019 Marcus Stollsteimer
+# Copyright (C) 2013-2020 Marcus Stollsteimer
 #
 # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 
