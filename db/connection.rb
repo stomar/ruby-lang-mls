ba7aa7aac52828f137cdcef8d2cd761be8a54271
Marcus Stollsteimer <sto.mar@web.de>
2020-01-23 20:19:29 +0100

    Move setup of test database into test helper

--- db/connection.rb
+++ db/connection.rb
@@ -6,11 +6,6 @@ DATABASE_URL = ENV["DATABASE_URL"] || "sqlite://#{Dir.pwd}/db/development.db"
 
 begin
   DB = Sequel.connect(DATABASE_URL)
-
-  # create tables for in-memory database
-  if ENV["DATABASE_URL"] == "sqlite:/"
-    load File.expand_path("schema.rb", __dir__)
-  end
 rescue StandardError, LoadError => e
   warn "Error initializing database: #{e.class}: #{e}"
   DB = false
--- test/helper.rb
+++ test/helper.rb
@@ -8,6 +8,8 @@ require_relative "../app"
 
 
 def setup_database
+  load File.expand_path("../db/schema.rb", __dir__)
+  load File.expand_path("../db/models.rb", __dir__)
   load File.expand_path("fixtures.rb", __dir__)
 end
 
