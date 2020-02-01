ff70edbedd0f9ce8da10e37354b1bfb3794d0983
Marcus Stollsteimer <sto.mar@web.de>
2020-02-01 22:32:55 +0100

    Simplify some test cases

--- test/test_logger.rb
+++ test/test_logger.rb
@@ -16,25 +16,25 @@ describe MLS::Logger do
 
   it "can log an entry (success)" do
     capture_io do
-      @logger.log("ruby-talk", "test success")
+      @logger.log("ruby-talk", "test")
     end
 
-    _(Log.last.to_s).must_match "Success (ruby-talk, test success)"
+    _(Log.last.to_s).must_match "Success (ruby-talk, test)"
   end
 
   it "can log an entry (invalid)" do
     capture_io do
-      @logger.log_invalid("ruby-talk", "test invalid")
+      @logger.log_invalid("ruby-talk", "test")
     end
 
-    _(Log.last.to_s).must_match "Invalid (ruby-talk, test invalid)"
+    _(Log.last.to_s).must_match "Invalid (ruby-talk, test)"
   end
 
   it "can log an entry (error)" do
     capture_io do
-      @logger.log_error("ruby-talk", "test error", RuntimeError.new("message"))
+      @logger.log_error("ruby-talk", "test", RuntimeError.new("message"))
     end
 
-    _(Log.last.to_s).must_match %r{Error +\(ruby-talk, test error\) RuntimeError\z}
+    _(Log.last.to_s).must_match %r{Error +\(ruby-talk, test\) RuntimeError\z}
   end
 end
