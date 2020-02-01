671d242c1459abcc4c258d36199b9132022aa20b
Marcus Stollsteimer <sto.mar@web.de>
2020-02-01 19:52:16 +0100

    Improve fixtures
    List log entries for errors in wrong order, to make
    sure that the sorting by date is properly tested.

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -4,10 +4,10 @@ fixtures = {}
 
 fixtures[:log] = [
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 2), status: "Success", list: "ruby-talk", action: "test" },
-  { timestamp: Time.utc(2000, 1, 2, 12, 0, 3), status: "Error",   list: "ruby-talk", action: "test_error" },
+  { timestamp: Time.utc(2000, 1, 2, 12, 0, 4), status: "Invalid", list: "ruby-talk", action: "test_invalid" },
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 6), status: "Success", list: "ruby-talk", action: "test_last" },
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 1), status: "Success", list: "ruby-talk", action: "test_first" },
-  { timestamp: Time.utc(2000, 1, 2, 12, 0, 4), status: "Invalid", list: "ruby-talk", action: "test_invalid" },
+  { timestamp: Time.utc(2000, 1, 2, 12, 0, 3), status: "Error",   list: "ruby-talk", action: "test_error" },
   { timestamp: Time.utc(2000, 1, 2, 12, 0, 5), status: "Success", list: "ruby-talk", action: "test" }
 ]
 
