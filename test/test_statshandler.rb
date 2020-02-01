41d14a2b365375465eef1e6832c154e2dcb35f8b
Marcus Stollsteimer <sto.mar@web.de>
2020-02-01 18:55:33 +0100

    Prefer using DailyStats model for test queries

--- test/test_statshandler.rb
+++ test/test_statshandler.rb
@@ -17,19 +17,22 @@ describe MLS::StatsHandler do
   it "can increment an existing entry (1)" do
     @stats.increment("ruby-talk", "subscribe", timestamp: Time.utc(2000, 1, 2, 12, 0, 10))
 
-    _(@stats.entries.last.to_s).must_match "2000-01-02,5"
+    stats = DailyStats.first(date: Date.new(2000, 1, 2)).to_s
+    _(stats).must_match "2000-01-02,5"
   end
 
   it "can increment an existing entry (2)" do
     @stats.increment("ruby-core", "unsubscribe", timestamp: Time.utc(2000, 1, 2, 12, 0, 10))
 
-    _(@stats.entries.last.to_s).must_match "2000-01-02,4,0,0,1"
+    stats = DailyStats.first(date: Date.new(2000, 1, 2)).to_s
+    _(stats).must_match "2000-01-02,4,0,0,1"
   end
 
   it "can increment a non-existing entry" do
     @stats.increment("ruby-talk", "subscribe", timestamp: Time.utc(2010, 1, 1))
 
-    _(@stats.entries.last.to_s).must_match "2010-01-01,1"
+    stats = DailyStats.first(date: Date.new(2010, 1, 1)).to_s
+    _(stats).must_match "2010-01-01,1"
   end
 
   it "can increment a non-existing entry for today" do
@@ -38,11 +41,12 @@ describe MLS::StatsHandler do
       @stats.increment("ruby-talk", "unsubscribe")
     end
 
-    _(@stats.entries.last.to_s).must_match "2010-01-01,0,1"
+    stats = DailyStats.first(date: Date.new(2010, 1, 1)).to_s
+    _(stats).must_match "2010-01-01,0,1"
   end
 
   it "can return all entries in correct order" do
-    _(@stats.entries.size).must_equal 2
+    _(DailyStats.count).must_equal 2
     _(@stats.entries[0].to_s).must_match "2000-01-01,7"
     _(@stats.entries[1].to_s).must_match "2000-01-02,4"
   end
