ad91cf1e37daccec5e523659632ce2b209b1f834
Marcus Stollsteimer <sto.mar@web.de>
2023-12-26 18:01:48 +0100

    Prefer string interpolation to string concatenation

--- models/log.rb
+++ models/log.rb
@@ -17,7 +17,7 @@ class Log < Sequel::Model
   def to_s
     msg =  timestamp.strftime("[%Y-%m-%d %H:%M:%S %z] ")
     msg << status.ljust(7)
-    msg << " (#{(list + ',').ljust(10)} #{action})"
+    msg << " (#{"#{list},".ljust(10)} #{action})"
     msg << " #{exception}"  if exception
 
     msg
